a2d94d8111a59e1d00e8ed4857ccccecf2a91d51
NimheAdler <carlos9rg@gmail.com>
2021-12-14 18:26:11 +0100

    v2

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ let projectData = {};
 
 const bodyParser = require('body-parser');
 
+// Require Express to run server and routes
 const express = require('express');
-
 const app = express();
 
+// Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 
@@ -17,15 +18,16 @@ app.use(bodyParser.json());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-// Setup Server
-
+// Setup Server + callback function
 const port = 3000;
 const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
 
+//get route
 app.get('/get', (req, res)=> {
   res.send(projectData);
 });
 
+//post route
 app.post('/', (req, res)=> {
     projectData = req.body;
     console.log (projectData);
--- website/app.js
+++ website/app.js
@@ -2,9 +2,11 @@
 let d = new Date();
 let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+
 const link = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const api = ',us&appid=c26f04150224362d9468395236f55e36&units=Metric';
 
+/* Function to generate the Api,  */
 function generateApi (e) {
     fetch(link+document.getElementById('zip').value+api).then(response => response.json())
     .then(response => {
@@ -21,6 +23,8 @@ function generateApi (e) {
             }),
         });
     })
+
+    /* Promise to GET Project Data */
     .then(() =>{
         return fetch('http://localhost:3000/get', {
             method: 'GET',
@@ -31,12 +35,22 @@ function generateApi (e) {
         })
     })
     .then(response => response.json())
+
+    // Write updated data to DOM elements
     .then(response => {
         document.getElementById('date').innerHTML = response.date;
         document.getElementById('temp').innerHTML = response.temperature + ' &#176C';
         document.getElementById('content').innerHTML = response.feeling;
     })
 
+    //Shows error if Zip is invalid (clearing previous inputs).
+    .catch(error => {
+        console.log("error", error);
+        document.getElementById('date').innerHTML = 'ERROR';
+        document.getElementById('temp').innerHTML = '';
+        document.getElementById('content').innerHTML = '';
+        })
+        
 }
 
 document.getElementById('generate').addEventListener('click', generateApi)
